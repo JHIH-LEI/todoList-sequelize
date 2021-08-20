435a0bd1d173956d7a134021041e711395fe01c7
JHIH-LEI <vicky88888@gmail.com>
2021-08-20 19:19:46 +0800

    project init

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const session = require('express-session')
+
+const PORT = 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'test',
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+app.get('/', (req, res) => {
+  res.send('hi')
+})
+
+app.listen(PORT, () => {
+  console.log(`express is running on http://localhost:${PORT}`)
+})
