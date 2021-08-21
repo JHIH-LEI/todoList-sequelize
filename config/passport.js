4d4d931be24769d6582d8a1673d603b84bd45363
JHIH-LEI <vicky88888@gmail.com>
2021-08-22 04:14:46 +0800

    feat: add facebook login

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const FacebookStrategy = require('passport-facebook')
 const db = require('../models')
 const User = db.User
 const bcrypt = require('bcryptjs')
@@ -24,6 +25,32 @@ module.exports = (app) => {
             })
         })
         .catch(err => done(err, false))
+    }
+  ));
+
+  passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: process.env.FACEBOOK_APP_CALLBACK,
+    profileFields: ['email', 'displayName']
+  },
+    (accessToken, refreshToken, profile, done) => {
+      const { email, name } = profile_json
+      User.findOne({ where: { email } })
+        .then(user => {
+          if (user) {
+            return done(null, user)
+          }
+          const randomPassword = Math.random().toString(36).slice(-8)
+          return bcrypt
+            .genSalt(10)
+            .then(salt => bcrypt.hash(randomPassword, salt))
+            .then(hash => {
+              User.create({ email, name, password: hash })
+            })
+            .then(user => done(null, user))
+            .catch(err => done(err, false))
+        })
     }
   ));
   // 反/序列化
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router()
 const home = require('./modules/home')
 const user = require('./modules/users')
 const todo = require('./modules/todos')
+const auth = require('./modules/auth')
 const { authenticator } = require('../middleware/auth')
 
 router.use('/todos', authenticator, todo)
 router.use('/users', user)
+router.use('/auth', auth)
 router.use('/', authenticator, home)
 
 module.exports = router
